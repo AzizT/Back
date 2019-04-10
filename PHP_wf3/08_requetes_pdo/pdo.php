9dd781c95f08e482577211290a30adcecb7fdca1
A.Tobbal <azziz.tobbal@lepoles.com>
2019-04-10 16:25:04 +0200

    BACK

--- PHP_wf3/08_requetes_pdo/pdo.php
+++ PHP_wf3/08_requetes_pdo/pdo.php
@@ -65,6 +65,7 @@
 
         $resultat = $pdo->exec("UPDATE employes SET salaire = 1300 WHERE id_employes = 350 ");
         echo "nombre d' enregistrement affecté(s) par l' UPDATE : $resultat <br>";
+        // $resultat contient un integer. Il garde en mémoire le nombre de modifications. Mais on ne peut l' associer directement a exec ou query. Par contre on lm' appelle dans l' echo pour "déstocker" l' integer qu' il a en  mémoire dont on a besoin
 
         // DELETE supprimer employé 350
 
@@ -76,6 +77,7 @@
         $result = $pdo->query("SELECT*FROM employes WHERE id_employes = 415");
         // query permet de selectionner, appeler des données ( alors que exec ne le peut pas)
         // utiliser query pour select et exec pour insert, update, delete
+        // query peut faire un insert, delete etc...sauf que lui ne peut afficher le résultat des modifs => mieux utiliser exec donc
 
         echo '<pre>';
         var_dump($result);
@@ -114,7 +116,7 @@
         var_dump($resultat);
         echo "</pre>";
 
-        // en executant une requete de selection, on obtient en retour un objet PDOStatement. Cet objet est inexploitable en l' état. On lui associe donc la methode FETCH qui retourne un tableau.
+        // en executant une requete de selection, on obtient en retour un objet PDOStatement. Cet objet est inexploitable en l' état. On lui associe donc la methode FETCH / fetchAll (Class PDOStatement) qui retournent un tableau ( Fetch => tableau array/ FetchAll => tableau multidimentionnel)
         // pour récupérer l' ensemble des employés, dans le cas qui nous occupe ( masi pas tout le temps donc), on va créer une boucle
         // $employes receptionne un tableau array d' un employé par tour de boucle
         while ($employes = $resultat->fetch(PDO::FETCH_ASSOC))
@@ -132,7 +134,7 @@
                 echo '</div>';
             }
 
-        echo '<hr><h2 class="display-4 text-center">05. PDO : QUERY + FETCHALL + FETCH_ASSOC (plusieurs résultats</h2><hr>';
+        echo '<hr><h2 class="display-4 text-center">05. PDO : QUERY + FETCHALL + FETCH_ASSOC (plusieurs résultats)</h2><hr>';
 
         $resultat = $pdo->query("SELECT * FROM employes");
 
@@ -156,10 +158,154 @@
 
         // exo: afficher la liste des bases de données, puis les mettre dans une liste ul li
 
-        
+        $resultat = $pdo->query("SHOW DATABASES");
+        echo '<pre>';
+        print_r($resultat);
+        echo '</pre>';
+        echo '<ul class="col-md-4 offset-md-4 mx-auto list-group text-center">';
+
+        while ($data = $resultat->fetch(PDO::FETCH_ASSOC)) {
+            // echo '<pre>'; print_r($data); echo '</pre>';
+            echo '<li class="list-group-item">' . $data['Database'] . '</li>';
+        }
+        echo '</ul>';
 
 
 
+        // Methode Djamila, qui reprend la logique précédente, + compréhensible
+        // $resultat = $pdo->query("SHOW DATABASES");// je cree ma variable resultat
+
+        // $donnee = $resultat->fetchALL(PDO::FETCH_ASSOC);
+
+        // echo "<pre>"; print_r($donnee); echo"</pre>";
+
+        // echo '<ul>';
+
+        // foreach($donnee as $key=> $tab1)
+
+        // {
+
+        //     foreach($tab1 as $key2=> $value){
+
+        //         echo  '<li>'; 
+
+        //          echo "$key2 : $value";
+
+        //           echo '</li>';
+
+        //     }
+
+
+
+
+
+        //   }
+
+        // echo'</ul>';
+
+        echo '<hr><h2 class="display-4 text-center">07. PDO : QUERY + TABLE </h2><hr>';
+
+        /*
+        columnCount est une méthode/fonction de la class PDOStatement qui retourne le nombre de colonne selectionnées via la requete de selection.
+        Dans notre cas, elle va retourner integer 7, donc la  boucle for va tourner 7 fois ( autant de fois qu' il y a de colonnes)
+
+        getColumnMeta() est une methode issue de la class PDOStatement qui permet de récolter les informations des champs/ colonnes selectionnées
+        */
+        $resultat = $pdo->query("SELECT * FROM employes");
+        echo '<table class="table table-bordered text-center"><tr>';
+        for ($i = 0; $i < $resultat->columnCount(); $i++) {
+            $colonne = $resultat->getColumnMeta($i);
+            // echo "<pre>";  print_r($colonne);   echo "</pre>";
+            echo "<th>$colonne[name]</th>";
+            // on crochete a l' indice [name] pour afficher en entete le nom de la colonne par tour de boucle
+        }
+
+        echo '</tr>';
+        while ($employe = $resultat->fetch(PDO::FETCH_ASSOC))
+            // $employe receptionne un tableau array par employés par tour de boucle
+            {
+                // echo "<pre>";  print_r($employe);   echo "</pre>";
+                echo '<tr>';
+                foreach ($employe as $value)
+                    // la boucle foreach permet de parcourir chaque tableau array de chaque employé
+                    {
+                        echo "<td>$value</td>";
+                    }
+
+                echo '</tr>';
+            }
+        echo '</table>';
+
+        // exo: faire la même chose en utilisant fetchall
+
+        $resultat = $pdo->query("SELECT * FROM employes");
+        $employes = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
+        echo "<pre>";
+        print_r($employe);
+        echo "</pre>";
+
+        echo '<table class="table table-bordered text-center"><tr>';
+
+        foreach ($employes[0] as $key => $value)
+            // on crochete au premier indice, [0], du tableau milti pour récupérer les indices et les stocker dans les entetes <th></th>
+            {
+                echo "<th>$key</th>";
+            }
+
+        echo '</tr>';
+        foreach ($employes as $tab) {
+                echo '<tr>';
+                // on crée une ligne par employé
+                foreach ($tab as $infos)
+                    // cette boucle permet de parcourir chaque tableau ARRAY de chaque employé
+                    {
+                        echo "<td>$infos</td>";
+                    }
+                echo '</tr>';
+            }
+        echo '</table>';
+
+        echo '<hr><h2 class="display-4 text-center">08. PDO : PREPARE + BINDVALUE + EXECUTE </h2><hr>';
+
+        // les requetes préparées permettent de formuler une seule fois la requete, puis de l' executer autant de fois que souhaité
+        // 3 cycles dans une requete: analyse / interprétation / execution
+        // les requetes préparées permettent de parer aux injections sql, cela permet de protéger les requetes sql
+
+        $resultat = $pdo->prepare("SELECT*FROM employes WHERE prenom = :prenom");
+        // ici on prépare la requzete, mais on ne l' execute pas encore
+        // :nom => marqueur nominatif que l' on peut comparer a une boite ou un tampon
+
+        echo "<pre>";  print_r($resultat);   echo "</pre>";
+        $resultat->bindvalue(':prenom', 'Aziz', PDO::PARAM_STR);
+        // bindvalue(), methode PDOStatement, permet d' associer une valeur au marqueur nominatif ':nom'
+        // arguments bindvalue(nom_du_marqueur, valeur, type)
+        // a ce stade, la requete n' est toujours pas  executée
+
+        $resultat->execute();
+        // execute() est une methode PDOStatement qui permet d' executer la requete préparée
+        echo "<pre>";  print_r($resultat);   echo "</pre>";
+
+        $employe = $resultat->fetch(PDO::FETCH_ASSOC);
+
+        echo "<pre>"; print_r($employe);  echo "</pre>";
+            echo '<div class="col-md-4 offset-md-4 mx-auto alert alert-info text-center ">';
+        foreach($employe as $key => $value)
+        {
+            echo "$key : $value<hr>";
+        }
+
+        echo '</div><hr>';
+
+        $prenom = 'Yannis';
+        // la valeur du marqueur peut-etre une valeur
+        $resultat->bindvalue(':prenom', $prenom, PDO::PARAM_STR);
+        // on change la valeur du marqueur sans avoir a reformuler la requete sql
+        $resultat->execute();
+        // execution de la nouvelle requete
+        $employe = $resultat->fetch(PDO::FETCH_ASSOC);
+
+        echo "<pre>"; print_r($employe);  echo "</pre>";
 
 
         ?>
---
+++ PHP_wf3/09_tchat/tchat.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Tchat</title>
+
+    <!-- link bootstrap -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <!-- exo : espace de dialogue (tchat)
+1 modélisation et création d' une
+bdd tchat
+Table : commentaire        
+        id_commentaire     // INT(3) PK - AI
+        pseudo             // VARCHAR(20)
+        dateEnregistrement // DateTime
+        message            // TEXT
+2 connexion a la bdd
+3 creation d' un formulaire html ( pourl' ajourt de message)
+4 recuperation et affichage des saisies en PHP ($_POST)
+5 requete SQL d' enregistrement ( INSERT)
+6 affichage des messages
+-->
+
+    <div class="container">
+
+        <?php
+        // connexion bdd
+        $bdd = new PDO('mysql:host=localhost;dbname=tchat', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+        // recup des données en php
+        // echo '<pre>';
+        // print_r($_POST);
+        // echo '</pre>';
+
+        // Je prépare l'insertion des valeur saisies dans les champs du formulaire
+        extract($_POST);
+        // permet de transformer chaque indice du formulaire en valeur
+        if ($_POST) {
+            // $resultat = $bdd->exec("INSERT INTO commentaire (pseudo,dateEnregistrement,message) VALUES ('$pseudo',NOW(),'$message')");
+            // // NOW permet l' insertion automatique de l' heure qu' il est
+            // echo "nombre d' enregistrement: $resultat<br>";
+
+            $resultat = $bdd->prepare("INSERT INTO commentaire (pseudo, dateEnregistrement, message) VALUES (:pseudo, NOW(), :message)");
+            $resultat->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+            $resultat->bindValue(':message', $message, PDO::PARAM_STR);
+            $resultat->execute();
+            // ces lignes de codes, la methode prepare(), permet d' éviter les injections sql des hackers
+            // la ok'); DELETE FROM commentaire;( ne fonctionne plus ( supprimait les commentaires définitivement)
+            /* 
+                Injection SQL:
+                ok'); DELETE FROM commentaire;(
+                */
+        }
+
+        $resultat = $bdd->query("SELECT pseudo, message, DATE_FORMAT (dateEnregistrement, '%d/%m/%Y') AS datefr, DATE_FORMAT (dateEnregistrement, '%H:%i:%S') AS heurefr FROM commentaire ORDER BY dateEnregistrement DESC");
+
+        while ($commentaire = $resultat->fetch(PDO::FETCH_ASSOC)) {
+                // echo '<pre>'; print_r($commentaire); echo '</pre>';
+                echo '<div class="col-md-8 offset-md-2 alert alert-secondary">';
+
+                echo "<div><em>Par $commentaire[pseudo], le $commentaire[datefr], à $commentaire[heurefr]</em></div><hr>";
+                echo "<div class='text-justify'>$commentaire[message]</div>";
+
+                echo '</div>';
+            }
+
+        echo "<div class='text-center'>Nombre de commentaire(s) :<strong class='badge badge-danger'> " . $resultat->rowCount() . '</strong></div>';
+        // rowCount est une methode PDOStatement qui retourne le nombre de lignes résultant de la requete SELECT
+
+        ?>
+
+        <h2 class="display-4 text-center">Dialogue Tchat</h2>
+
+        <form class="col-md-4 offset-md-4 text-center" method="post" action="">
+
+            <div class="form-group">
+                <label for="pseudo">Votre pseudo</label>
+                <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="pseudo">
+            </div>
+
+            <div class="form-group">
+                <label for="message">Votre message</label>
+                <textarea class="form-control" id="message" name="message" rows="3"></textarea>
+            </div>
+
+            <!-- le bouton submit -->
+            <button type="submit" class="btn btn-primary">Sign in</button>
+
+
+        </form>
+
+
+
+    </div>
+
+
+    <!-- bibliotheque pour bootstrap -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea 6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
