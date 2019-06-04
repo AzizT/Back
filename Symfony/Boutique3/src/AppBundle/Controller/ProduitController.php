7036646590d01f39df35cbe9c100857b34b1a5ae
A.Tobbal <azziz.tobbal@lepoles.com>
2019-06-04 16:52:00 +0200

    affichage sommaire boutique sf

--- Symfony/Boutique3/src/AppBundle/Controller/ProduitController.php
+++ Symfony/Boutique3/src/AppBundle/Controller/ProduitController.php
@@ -13,6 +13,31 @@ class ProduitController extends Controller
      */
     public function indexAction()
     {
-        
+        $params = array();
+        return $this->render('@App/Produit/index.html.twig', $params);
+    }
+
+    /**
+     * @Route("/produit/{id}", name="produit")
+     * www.maboutique.com/produit/12
+     */
+
+     public function produitAction($id)
+     {
+        $params = array(
+            'id' => $id
+        );
+        return $this -> render('@App/Produit/show.html.twig', $params);
+     }
+
+    /**
+     * @Route("/categorie/{cat}", name="categorie")
+     * www.maboutique.com/categorie/tshirt
+     */
+
+    public function categorieAction($cat)
+    {
+        $params = array();
+        return $this -> render('@/Produit/index.html.twig', $params);
     }
 }
